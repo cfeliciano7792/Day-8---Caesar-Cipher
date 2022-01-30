04001a3b63a9508d7695ae889c8d2e27af4cbaca
Christian F <96458958+cfeliciano7792@users.noreply.github.com>
2022-01-30 20:46:15 +0000

    changed second if to elif in line 39

--- main.py
+++ main.py
@@ -36,5 +36,5 @@ def decrypt(cipher_text, shift_amount):
 
 if direction == "encode":
   encrypt(plain_text = text, shift_amount = shift)
-if direction == "decode":
+elif direction == "decode":
   decrypt(cipher_text = text, shift_amount = shift)
