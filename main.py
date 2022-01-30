173a451b036aa7cb1557053a565b3a920ec85cb7
Christian F <96458958+cfeliciano7792@users.noreply.github.com>
2022-01-30 20:36:04 +0000

    Added second function to decode text. Added if statement to determine if user wants to encode or decode

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
@@ -15,10 +15,26 @@ def encrypt(plain_text, shift_amount):
     letter_position = alphabet.index(letter)
     #shifts the letter thats outputted based on user input
     letter_shift = alphabet[letter_position + shift]
-    continue
     shifted_string += letter_shift
   print(f"The encoded text is {shifted_string}")
 
+def decrypt(plain_text, shift_amount):
+   #creates empty string
+  shifted_string = ""
+
+  for letter in plain_text:
+  #creates an index of item in the list alphabet. Ex a = 0 b = 1 etc
+    letter_position = alphabet.index(letter)
+  #shifts the letter thats outputted based on user input
+    letter_shift = alphabet[letter_position - shift]
+    shifted_string += letter_shift
+  print(f"The decoded text is {shifted_string}")
+  
     ##HINT: How do you get the index of an item in a list:
     #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-encrypt(plain_text = text, shift_amount = shift)
+
+
+if direction == "encode":
+  encrypt(plain_text = text, shift_amount = shift)
+if direction == "decode":
+  decrypt(plain_text = text, shift_amount = shift)
