d1b556d096389c521f3a938f46fac1935fb3a463
Christian F <96458958+cfeliciano7792@users.noreply.github.com>
2022-01-30 18:40:19 +0000

    removed # in line 3

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-#direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
 
-def encrypt(text, shift):
+def encrypt(plain_text, shift_amount):
   #creates empty string
   shifted_string = ""
 
-  for letter in text:
+  for letter in plain_text:
     #creates an index of item in the list alphabet. Ex a = 0 b = 1 etc
-    let_num = alphabet.index(letter)
+    letter_position = alphabet.index(letter)
     #shifts the letter thats outputted based on user input
-    let_shift = alphabet[let_num + shift]
-    shifted_string += let_shift
+    letter_shift = alphabet[letter_position + shift]
+    shifted_string += letter_shift
   print(f"The encoded text is {shifted_string}")
 
-encrypt(text, shift)
+encrypt(plain_text = text, shift_amount = shift)
 
     
     
@@ -28,4 +28,3 @@ encrypt(text, shift)
 
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
